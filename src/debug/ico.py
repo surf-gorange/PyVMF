16174955baf6edd51c1d8e4bbc325bebcf3bf5fc
GorangeNinja <gorange.ninja@gmail.com>
2020-02-19 18:17:32 +0100

    Added material referencing to obj files

--- src/debug/ico.py
+++ src/debug/ico.py
@@ -3,7 +3,7 @@ from obj import *
 
 v = new_vmf()
 
-for solid in obj_to_solids("ico.obj", 16):
+for solid in obj_to_solids("test.obj", "gorange/objtest", 14.5):
     v.add_solids(solid)
 
 v.export("ico.vmf")
--- src/obj.py
+++ src/obj.py
@@ -2,13 +2,17 @@ from pywavefront import *
 from PyVMF import *
 
 
-def obj_to_solids(filename: str, scale: int = 64):
+def obj_to_solids(filename: str, material_path: str = "", scale=64):
     """
     Turns an .obj file to VMF solids, **BETA** it's very finicky and remember to invert normals
 
-    :param filename:
-    :param scale:
+    :param filename: The name of the .obj file with path (ex: "test/wall.obj")
+    :param material_path: The path to the .VMT's using same names (from the game materials folder, ex: "custom/ramp/"
+    :param scale: The scale applied to the entire .obj
     """
+    if material_path[-1] != "/":
+        material_path += "/"
+
     scene = Wavefront(filename, collect_faces=True)
     for mesh in scene.mesh_list:
         solid = Solid()
@@ -25,6 +29,7 @@ def obj_to_solids(filename: str, scale: int = 64):
             solid.add_sides(side)
 
         solid.editor = Editor()
+        solid.set_texture(material_path + mesh.materials[0].name[0:-3])
         solid.rotate_x(Vertex(0, 0, 0), 90)
 
         yield solid
